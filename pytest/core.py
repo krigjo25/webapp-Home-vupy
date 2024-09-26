c67e4751518a84326875c4baa4c2e85f70414ebf
Kristoffer Gjøsund <73070534+krigjo25@users.noreply.github.com>
2024-09-26 09:43:55 +0200

    IC

--- pytest/core.py
+++ pytest/core.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #   errorHandler
-from errorHandler import TableError
+from errorHandler import OperationalError
 
 #   Requests repositories
 from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
@@ -137,8 +137,8 @@ class SQL(Base):
             except: sql = False
 
             #   Ensure that table does not exists and statements is a known keyword
-            if statement.upper() not in self.statements: raise TableError(404)
-            if table in tables and statement == 'CREATE': raise TableError(200)
+            if statement.upper() not in self.statements: raise OperationalError(404)
+            if table in tables and statement == 'CREATE': raise OperationalError(200)
 
             self.cur.execute(self.configure_table(table, statement, columns))
             self.conn.commit()
@@ -156,7 +156,7 @@ class SQL(Base):
             tables = self.cur.execute('SELECT name FROM sqlite_master').fetchall()
     
             #   Ensure table exists
-            if table not in tables[0]: raise TableError(404)
+            if table not in tables[0]: raise OperationalError(404)
             
             if not isinstance(data, list): raise SyntaxError('accepts only lists as argument')
 
