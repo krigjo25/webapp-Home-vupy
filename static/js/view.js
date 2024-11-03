d18fc86a02a153eee87fd83efd8550d881bc5ebc
krigjo25 <krigjo25@gmail.com>
2024-11-03 16:28:00 +0100

    Improvements

--- static/js/view.js
+++ static/js/view.js
@@ -4,41 +4,55 @@ function main()
 {
     //  Initialize variables
     let app = model.apps;
-    console.log(app);
+
     //  Linear algorithme
     for (let i = 0; i < app.length; i++)
     {
 
         //  Ensure the name is carosel
-        if (app[i].name === "carosel")
+        if (app[i].app === "carosel")
         {
-            
-            app[i].id.innerHTML = /*HTML*/ `
-                <img id="car-img" src="${app[i].path + app[i].source}" alt="${app[i].alt}" class="css-img">
-                <div class="caption">${app[i].caption}</div>
-                <div>
-                    <div class="active"></div>
-                </div>`;
+            app[i].id.innerHTML = /*HTML*/ ` 
+                ${carosel(app[i])}`;
         }  
-    }
-    about();
-    return;
-}
-
-function about()
-{
-    for (let i = 0; i < bio.length; i++)
-    {
-        if (bio[i].app.id == apps[1])
+        else if (app[i].app === "bio")
         {
-            bio[i].app.innerHTML = /*HTML*/`
-                <h2>${bio[i].title}</h2>
-                <span class="time">written by @krigjo25 , but formulated by Google's Gimini. read time ${bio[i].time}<i class="bi bi-stop-watch"></i></span>
-                <div class="grid-container">  
-                    <p class="grid-item">${bio[i].msg}</p>
-                    <p class="grid-item">${bio[i].msg1}</p>
-                    <p class="grid-item">${bio[i].msg2}</p>
-                </div>`;
+            app[i].id.innerHTML += /*HTML*/ ` 
+            ${about(app[i])}`;
         }
     }
 }
+
+function about(arg)
+{
+    console.log(arg);
+    arg.id.innerHTML = /*HTML*/`
+    <h2>${arg.name}</h2>
+    <span class="time">
+        written by @krigjo25 , but formulated by 
+        Google's Gimini. read time 
+        ${arg.time}
+        <i class="bi bi-stop-watch"></i>
+    </span>
+    <div class="grid-container">
+        <p class="grid-item">${arg.message}</p>
+        <p class="grid-item">${arg.message1}</p>
+        <p class="grid-item">${arg.message2}</p>
+    </div>`;
+}
+function carosel(arg)
+{
+    return  /*HTML*/`
+        <img id="car-img" src="${arg.path + arg.source}" alt="${arg.alt}" class="css-img">
+            <div class="caption">${arg.caption}</div>
+            <div id="img-btn" class="img-btn">
+                <button id ="prev-btn" class="prev-btn" onclick="prev()">
+                    <i class="bi bi-arrow-left-square-fill"></i>
+                </button>
+                <button id ="next-btn" class="next-btn"onclick="next()">
+                    <i class="bi bi-arrow-right-square-fill"></i>
+                </button>
+        </div>`;
+}
+
+
