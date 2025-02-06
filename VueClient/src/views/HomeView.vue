07b41f69d366949d782ac13b9386b7531e9704fa
Kristoffer <krigjo25@gmail.com>
2025-02-06 16:54:37 +0100

    IC

--- VueClient/src/views/HomeView.vue
+++ VueClient/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Carosel from '../components/carosel.vue'
+import Biography from '../components/bio.vue'
+import Education from '../components/education.vue'
+import Experience from '../components/experience.vue'
+import Footer from '../components/footer.vue'
+
 </script>
 
 <template>
-  <main>
+    <Carosel />
+    <Biography />
+    <Education />
+    <Experience />
+    <Footer />
     <TheWelcome />
-  </main>
 </template>
