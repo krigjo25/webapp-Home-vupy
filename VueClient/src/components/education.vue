9e49ace8e3a4574a068d6fea85f95989b09c6a5d
Kristoffer <krigjo25@gmail.com>
2025-02-06 16:41:29 +0100

    IC

--- VueClient/src/components/education.vue
+++ VueClient/src/components/education.vue
@@ -1,6 +1,6 @@
-{% with flash = get_flashed_messages()%}
-{% for i in flash %}
-<div class="flash">{{ i }}</div>
-{% endfor %}
-{% endwith %}
-<section id="specialization-container" class="spec-wrapper"></section> 
+<template>
+    <div>
+        <h1>Education</h1>
+        <p>Education is the process of facilitating learning, or the acquisition of knowledge, skills, values, beliefs, and habits. Educational methods include teaching, training, storytelling, discussion and directed research. Education frequently takes place under the guidance of educators, however learners can also educate themselves.</p>
+    </div>
+</template>
---
+++ ient/src/components/Gallery.vue
@@ -0,0 +1,17 @@
+<section id='about-kristoffer' class="info">
+                <section id="carosel-container" class="carosel">
+                </section>
+                <section id='bio'class="bio-container"></section>        
+                </div>
+                <nav class='ext-bar'>
+                    {% for key, value in links.items() %}
+
+                        <a href='{{ value }}'>
+                            <span>                           
+                                <i class="bi bi-{{ key }}"></i>
+                            </span>
+                        </a>
+                    {% endfor %}
+                </nav>
+                
+            </section>
