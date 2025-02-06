e58e8eb919023222d0396000d64c423c414f3a71
Kristoffer <krigjo25@gmail.com>
2025-02-06 16:55:17 +0100

    Initializing vue

--- ient/src/components/education.vue
+++
@@ -1,6 +0,0 @@
-{% with flash = get_flashed_messages()%}
-{% for i in flash %}
-<div class="flash">{{ i }}</div>
-{% endfor %}
-{% endwith %}
-<section id="specialization-container" class="spec-wrapper"></section> 
