fdba31ef5694060c4276b1c5db645a92c61fd56c
Kristoffer <krigjo25@gmail.com>
2025-05-12 20:12:10 +0200

    Minor improvements added a base class

--- flask/lib/utils/exception_handler.py
+++ flask/lib/utils/exception_handler.py
@@ -2,7 +2,21 @@
 
 from typing import Optional
 
-class OperationalError(Exception):
+
+class ExceptionHandler(Exception):
+    """ Base class for all exceptions """
+
+    def __init__(self, code:int, message:Optional[str]) -> None:
+        super().__init__(code, message)
+        self.status_code = code
+        self.message = message if message else "An error occurred"
+        
+
+        if message is None:
+            self.message = "An error occurred"
+
+
+class OperationalError(ExceptionHandler):
     """ Raises when duplicated is not allowed """
 
     error = {
@@ -11,24 +25,17 @@ class OperationalError(Exception):
         404:"Table does not exist in the database",
         500:'Column has to be a type of list'}
 
-    def __init__(self, code, message:Optional[str]) -> None:
-        
+    def __init__(self, code:int, message:Optional[str]) -> None:
         super().__init__(code, message)
         self.status_code = code
-        self.message = message
+        self.message = message if message else self.error[code]
 
-        if message is None:
-            self.message = self.error[code]
 
-class NotFoundError(Exception):
+class NotFoundError(ExceptionHandler):
     """ Raises when the requested resource is not found """
 
-    error = {404: "404: Resource not found"}
-
     def __init__(self, code, message:Optional[str]) -> None:
         super().__init__(code, message)
         self.status_code = code
-        self.message = message
-
-        if message is None:
-            self.message = self.error[code]
+        self.message = message if message else "Resource not found"
+        
